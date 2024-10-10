b618310e46ec848dddb70765282b5d37bcb1adbf
Hunter zaker <33567233+hassanzaker@users.noreply.github.com>
2024-10-10 12:42:02 -0400

    Update settings.py

--- movie_app/movie_app/settings.py
+++ movie_app/movie_app/settings.py
@@ -112,6 +112,7 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 AUTH_USER_MODEL = 'users.CustomUser'
 
 # CORS Configuration
+CSRF_COOKIE_DOMAIN = 'movielads.net'
 CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOW_ALL_ORIGINS = False
 CORS_ALLOWED_ORIGINS = [
